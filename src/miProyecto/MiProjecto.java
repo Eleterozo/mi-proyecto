d6d01941734268c3a3eed28b3e76f67c41281254
Eleterozo <martintemprano769@gmail.com>
2025-11-02 17:37:47 +0100

    primera version

---
+++ src/miProyecto/MiProjecto.java
@@ -0,0 +1,10 @@
+package miProyecto;
+
+public class MiProjecto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Macarrones con Queso");
+	}
+
+}
